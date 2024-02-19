cde2d4a5efe210b2dbe1fb19639102c19ee87bd7
Yusuf Can Anar <yusufcan.anar@tai.com.tr>
2024-02-19 14:21:03 +0300

    Makefile.common added this lines to TRICK_EXEC_LINK_LIBS -L/usr/local/lib -lpthread -lcrypto -lssl -lmongocxx -lbsoncxx Check other usages of thir party libraries with preprocessor directives like civerweb usage

--- trick_sims/Cannon/SIM_cannon_analytic/S_overrides.mk
+++ trick_sims/Cannon/SIM_cannon_analytic/S_overrides.mk
@@ -1,8 +1,8 @@
 TRICK_CFLAGS += -I../models -I/home/fjolnirr/trick/trick_source -g 
 TRICK_CXXFLAGS += -I../models -I/home/fjolnirr/trick/trick_source -g 			
 
-TRICK_EXEC_LINK_LIBS += -L/usr/local/lib -lpthread -lcrypto \
--lssl -lmongocxx -lbsoncxx
+# TRICK_EXEC_LINK_LIBS += -L/usr/local/lib -lpthread -lcrypto \
+# -lssl -lmongocxx -lbsoncxx
 
 # add the line below to the .bashrc for permanent solution
 # export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
